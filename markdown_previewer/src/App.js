34312004afa74721ee984cc5b62c29fbe3a3e221
Junior Dugue <jrdugue@gmail.com>
2019-03-30 23:05:37 -0400

    added css grid to container, and styled the textarea and preview area to be  responsive

--- markdown_previewer/src/App.js
+++ markdown_previewer/src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 const initialMarkdown = `
-###Headers
+### Headers
 
 # Header 1
 ## Header 2
 ### Header 3
 `
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      markdown: initialMarkdown
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <div>Markdown Previewer</div>
+          <h1>Markdown Previewer</h1>
           <div className="container">
             <div className="left">
               <textarea id="editor" />
             </div>
             <div className="right">
-              <div id="preview" />
+              <div id="preview" dangerouslySetInnerHTML={{__html: marked(this.state.markdown)}}/>
             </div>
           </div>
-        </header>
       </div>
     );
   }
